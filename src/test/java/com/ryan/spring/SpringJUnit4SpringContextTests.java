4192d75f4501ac0b856d28b480eea67a82f2e85d
Ryan-瑞恩 <liuwei41255703@163.com>
2017-03-16 17:41:55 +0800

    Spring 测试 Controller

---
+++ src/test/java/com/ryan/spring/SpringJUnit4SpringContextTests.java
@@ -0,0 +1,21 @@
+package com.ryan.spring;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <pre>
+ * User:        Ryan
+ * Date:        2017/3/16
+ * Email:       liuwei412552703@163.com
+ * Version      V1.0
+ * Discription: Spring 整体测试框架
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-*.xml"})
+public abstract class SpringJUnit4SpringContextTests extends AbstractTransactionalJUnit4SpringContextTests {
+
+
+}
