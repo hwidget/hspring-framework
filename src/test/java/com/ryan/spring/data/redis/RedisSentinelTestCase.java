1833e1680986ecc0b6864259a5f425018919e986
Ryan-瑞恩 <liuwei41255703@163.com>
2017-05-23 19:54:26 +0800

    测试 spring redis 哨兵模式。

---
+++ src/test/java/com/ryan/spring/data/redis/RedisSentinelTestCase.java
@@ -0,0 +1,45 @@
+package com.ryan.spring.data.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.*;
+
+/**
+ * @author Rayn
+ * @email liuwei412552703@163.com
+ * Created by Rayn on 2016/11/21 16:46.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:data/spring-data-redis-sentinel.xml"})
+public class RedisSentinelTestCase {
+    private static final Logger LOG = LoggerFactory.getLogger(RedisSentinelTestCase.class);
+
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+
+    @Test
+    public void testRandomWrite() throws Exception {
+        ValueOperations<String, String> operations = this.redisTemplate.opsForValue();
+
+        while (true){
+            operations.set(UUID.randomUUID().toString(), "1");
+
+            Thread.sleep(3 * 1000);
+        }
+
+    }
+}
